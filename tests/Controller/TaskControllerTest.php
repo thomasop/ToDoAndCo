13dcb73aaf331aeb7a5ab7556bfcb1e0c8322b86
thomasop <tdss33@hotmail.com>
2021-03-22 19:33:54 +0100

    Edit: fix doctrine override_url true

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -4,26 +4,15 @@ namespace Tests\App\Controller;
 
 use Symfony\Component\HTTPFoundation\Response;
 use Symfony\Component\Console\Input\StringInput;
-use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 
 class TaskControllerTest extends WebTestCase
 {
-    use FixturesTrait;
-
     private $client = null;
-
-    public function setUp(): void
-	{
-        $fixtures = $this->loadFixtures([
-            'App\DataFixtures\AppFixtures'
-        ])->getReferenceRepository();
-	}
 
     public function testListAction()
     {
-        self::ensureKernelShutdown();
         $this->client = static::createClient();
 
         $test = $this->client->request('GET', '/tasks');
