8331741f8dad2733fd72ebde19926da7954b5a1b
thomasop <tdss33@hotmail.com>
2021-04-02 10:20:51 +0200

    Edit: change entity test for coverage code

--- tests/Entity/TaskTest.php
+++ tests/Entity/TaskTest.php
@@ -4,8 +4,9 @@ namespace App\Tests\Entity;
 
 use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use PHPUnit\Framework\TestCase;
 
-class TaskTest extends KernelTestCase
+class TaskTest extends TestCase
 {
     /**
      * Test User Entity.
@@ -17,8 +18,15 @@ class TaskTest extends KernelTestCase
         $task = new Task();
         $task->setTitle('testtitle');
         $task->setContent('testcontent');
-        self::bootKernel();
-        $error = self::$container->get('validator')->validate($task);
-        $this->assertCount(0, $error);
+        $task->setCreatedAt(new \DateTime);
+        $this->assertEquals(date('Y-m-d H:i:s'), $task->getCreatedAt()->format('Y-m-d H:i:s'));
+        $this->assertEquals('testtitle', $task->getTitle());
+        $this->assertEquals('testcontent', $task->getContent());
+        $this->assertEquals(false, $task->IsDone());
+        $this->assertEquals(false, $task->toggle(false));
+
+        //self::bootKernel();
+        //$error = self::$container->get('validator')->validate($task);
+        //$this->assertCount(0, $error);
     }
 }
--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Entity;
 
 use App\Entity\User;
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserTest extends KernelTestCase
@@ -16,10 +17,20 @@ class UserTest extends KernelTestCase
     {
         $user = new User();
         $user->setUsername('testname');
+        $this->assertEquals('testname', $user->getUsername());
         $user->setPassword('testpassword');
+        $this->assertEquals('testpassword', $user->getPassword());
         $user->setEmail('tdss33@hotmail.com');
-        self::bootKernel();
-        $error = self::$container->get('validator')->validate($user);
-        $this->assertCount(0, $error);
+        $this->assertEquals('tdss33@hotmail.com', $user->getEmail());
+
+        $taskStub = $this->createMock(Task::class);
+        $user->addTask($taskStub);
+        $collection = $user->getTasks();
+        $this->assertEquals(false, $collection->isEmpty());
+        $user->removeTask($taskStub);
+        $this->assertEquals(true, $collection->isEmpty());
+        //self::bootKernel();
+        //$error = self::$container->get('validator')->validate($user);
+        //$this->assertCount(0, $error);
     }
 }
